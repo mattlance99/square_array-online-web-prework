aa22454e4dbedcd46c0960bc349c0a8a7e180c45
Matthew Cohen <matt.cohen182@gmail.com>
2018-10-15 22:24:27 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -3,7 +3,7 @@ def square_array(array)
   square_array = []
   square_num = 0 
   array.each do |num|
-    num ** 2
+    square_array << num ** 2
     #square_num = num ** 2
     square_array.push(square_num)
   end
